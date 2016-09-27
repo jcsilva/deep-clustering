2ac2bc79da714414ddde438a4f847111dcdd8d28
Eduardo Silva <zedudu@gmail.com>
2016-09-27 13:50:44 -0300

    Initial commit

---
+++ feats.py
@@ -0,0 +1,140 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 26 15:23:31 2016
+
+@author: jcsilva
+"""
+import numpy as np
+import math
+import scipy.io.wavfile as wav
+from python_speech_features import sigproc
+from keras.utils.np_utils import to_categorical
+
+FRAME_LENGTH = .032
+FRAME_SHIFT = .008
+FS = 8000
+
+CHUNK_SIZE = int((FRAME_LENGTH + 99 * FRAME_SHIFT) * FS)
+CHUNK_OVERLAP = CHUNK_SIZE / 2
+
+def squared_hann(M):
+    return np.sqrt(np.hanning(M))
+
+
+def stft(sig, rate):
+    frames = sigproc.framesig(sig,
+                              FRAME_LENGTH*rate,
+                              FRAME_SHIFT*rate,
+                              winfunc=squared_hann)
+    spec = np.fft.rfft(frames, int(FRAME_LENGTH*rate))
+    return np.log(np.absolute(spec))
+
+
+#def get_batches(batch_size):
+#    p1 = "LapsBM_0061.wav"
+#    rate, sig = wav.read(p1)
+#    sig = sig - np.mean(sig)
+#    sig = sig/np.max(np.abs(sig))
+#
+#    p2 = "LapsBM_0304.wav"
+#    rate2, sig2 = wav.read(p2)
+#    sig2 = sig2 - np.mean(sig2)
+#    sig2 = sig2/np.max(np.abs(sig2))
+#
+#    sig = sig[len(sig2)-6592:len(sig2)]
+#    sig2 = sig2[len(sig)-6592:len(sig)]
+#    x = sig + sig2
+#    
+#    SPEC = stft(sig, rate)
+#    SPEC2 = stft(sig2, rate)
+#    X = stft(x, rate)
+#    m = np.max(X) - 5.5 # consertar esse valor. Isso eh soh para identificar silencio!!
+#    y = np.zeros(X.shape, dtype=np.int16)
+#    y[SPEC > SPEC2] = 0
+#    y[SPEC2 >= SPEC] = 1
+#    y[X < m] = 2
+#    
+#    feats  = np.zeros((batch_size,) + X.shape)
+#    labels = np.zeros((batch_size,) + y.shape, dtype=np.int16)
+#    i = 0
+#    for k in range(1):
+#        feats[i] = X
+#        labels[i] = y
+#        i += 1
+#        if i == batch_size:
+#            i = 0
+#            yield(feats.reshape((batch_size, X.size)),
+#                  labels.reshape((batch_size, y.size)))
+#            feats = np.zeros((batch_size,) + X.shape)
+#            labels = np.zeros((batch_size,) + y.shape)
+
+def get_signals():
+    root_prefix = "/media/data/corpora/Laps/LapsBM1.4-8k/all/LapsBM_"   
+   
+    file_num = str(np.random.randint(1,700))
+    p1 = root_prefix + file_num.rjust(4,'0') + ".wav"
+    rate, sig = wav.read(p1)
+    sig = sig - np.mean(sig)
+    sig = sig/np.max(np.abs(sig))
+
+    file_num = str(np.random.randint(1,700))
+    p2 = root_prefix + file_num.rjust(4,'0') + ".wav"
+    rate2, sig2 = wav.read(p2)
+    sig2 = sig2 - np.mean(sig2)
+    sig2 = sig2/np.max(np.abs(sig2))
+
+    return sig, sig2    
+
+
+def myGenerator(n=0):
+    counter = 0 
+    
+    def condition(counter, n):
+        if n > 0:
+            return counter < n
+        else:
+            return True
+            
+    while condition(counter, n):
+        counter = counter + 1
+        s1, s2 = get_signals()
+        smallest_signal_len = len(s1) if len(s1) < len(s2) else len(s2)
+        num_chunks = math.floor( (smallest_signal_len - int(FRAME_LENGTH * FS)) / int(FRAME_SHIFT * FS) ) + 1
+        for i in range(num_chunks):
+            # seleciona um frame
+            sig  = s1[int(i*FRAME_SHIFT):int(i*FRAME_SHIFT) + CHUNK_SIZE]
+            sig2 = s2[int(i*FRAME_SHIFT):int(i*FRAME_SHIFT) + CHUNK_SIZE]
+            
+            # cria mistura            
+            x = sig + sig2
+            
+            # Espectro de magnitude
+            SPEC = stft(sig, FS)
+            SPEC2 = stft(sig2, FS)
+            X = stft(x, FS)
+            
+            # cria mascara binaria
+            m = np.max(X) - 5.5 # consertar esse valor. Isso eh soh para identificar silencio!!
+            y = np.zeros(X.shape, dtype=np.int16)
+            y[SPEC > SPEC2] = 0
+            y[SPEC2 >= SPEC] = 1
+            y[X < m] = 2
+            y = np.squeeze(y.reshape((1,-1)))
+            n_classes = np.max(y) + 1
+            
+            #if i%10==0:
+            #    print("i = " + str(i))
+            
+            # mlp
+            #yield (X.reshape((1,-1)),
+            #      to_categorical(y, NUM_CLASSES).reshape((1,-1)))
+            
+            # blstm
+            yield (X.reshape((1,X.shape[0], X.shape[1])),
+                  to_categorical(y, n_classes).reshape((1,-1)))
+
+
+if __name__ == "__main__":
+    a = myGenerator(1)
+    for i,j in a:
+        print(i.shape,j.shape)
---
+++ keras-speech-separation-batch.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 26 15:23:31 2016
+
+@author: jcsilva
+"""
+
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, LSTM, Input, Bidirectional
+from keras.layers.wrappers import TimeDistributed
+from keras.layers.noise import GaussianNoise
+from keras.optimizers import SGD
+from keras.models import model_from_json
+from feats import myGenerator
+
+EMBEDDINGS_DIMENSION = 50
+NUM_CLASSES = 3
+INPUT_SAMPLE_SIZE=12900
+
+def save_model(model, filename):
+    # serialize model to JSON
+    model_json = model.to_json()
+    with open(filename + ".json", "w") as json_file:
+        json_file.write(model_json)
+    #serialize weights to HDF5
+    model.save_weights(filename + ".h5")
+    print("Saved model to disk")
+
+
+def load_model(filename):
+    # load json and create model
+    json_file = open(filename + '.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    # load weights into new model
+    loaded_model.load_weights(filename + ".h5")
+    print("Loaded model from disk")
+    return loaded_model
+ 
+ 
+def affinitykmeans(Y, V):
+    def norm(tensor):
+        square_tensor = tf.square(tensor)
+        tensor_sum = tf.reduce_sum(square_tensor)
+        frobenius_norm = tf.sqrt(tensor_sum)
+        return frobenius_norm
+    
+    # V e Y estao vetorizados
+    # Antes de mais nada, volto ao formato de matrizes
+    V = tf.reshape(V, [-1, EMBEDDINGS_DIMENSION])
+    Y = tf.reshape(Y, [-1, NUM_CLASSES])
+   
+    T = tf.transpose
+    dot = tf.matmul
+    return norm(dot(T(V), V)) - 2 * norm(dot(T(V), Y)) + norm(dot(T(Y), Y))
+
+
+def train_nnet():       
+#    model = Sequential()
+#    model.add(Dense(64, input_dim=INPUT_SAMPLE_SIZE, init='uniform'))
+#    model.add(Activation('tanh'))
+#    model.add(Dropout(0.5))
+#    model.add(Dense(64, init='uniform'))
+#    model.add(Activation('tanh'))
+#    model.add(Dropout(0.5))
+#    model.add(Dense(INPUT_SAMPLE_SIZE * EMBEDDINGS_DIMENSION, init='uniform'))
+#    model.add(Activation('softmax'))
+
+    model = Sequential()
+    model.add(Bidirectional(LSTM(300, return_sequences=True), input_shape=(100,129)))
+    #model.add(Dropout(0.5))
+    #model.add(GaussianNoise(0.77))    
+    model.add(Bidirectional(LSTM(300)))    
+    #model.add(GaussianNoise(0.77))
+    #model.add(Dropout(0.5))
+    model.add(Dense(INPUT_SAMPLE_SIZE * EMBEDDINGS_DIMENSION, init='uniform', activation='tanh'))
+    #model.add(TimeDistributed(Dense(EMBEDDINGS_DIMENSION)))
+    #model.add(Activation('softmax'))
+
+    sgd = SGD(lr=1e-5, momentum=0.9, decay=0.0, nesterov=True)
+    model.compile(loss=affinitykmeans, optimizer=sgd)
+
+    model.fit_generator(myGenerator(),  samples_per_epoch=10, nb_epoch=1, max_q_size=10)
+    #score = model.evaluate(X_test, y_test, batch_size=16)
+    save_model(model, "model")
+    
+def main():
+    train_nnet()    
+    loaded_model = load_model("model")
+
+    ypred = []
+    for x,y in myGenerator(1):
+        ypred.append(loaded_model.predict(x))
+    #with open("outfile", "w") as f:
+    for y in ypred:
+        print(y.shape)
+
+
+if __name__ == "__main__":
+    main()
