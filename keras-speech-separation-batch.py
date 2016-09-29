1b7d5c6626fc55614dbd81fdcb7bbfef9bb49b4b
Eduardo Silva <zedudu@gmail.com>
2016-09-29 08:06:52 -0300

    Correção do formato do batch da LSTM

--- keras-speech-separation-batch.py
+++ keras-speech-separation-batch.py
@@ -5,22 +5,22 @@ Created on Mon Sep 26 15:23:31 2016
 @author: jcsilva
 """
 
-import tensorflow as tf
+from keras import backend as K
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation, LSTM, Input, Bidirectional
+from keras.layers import Dropout, Activation, Input, Reshape, BatchNormalization
+from keras.layers import Dense, LSTM, Bidirectional
 from keras.layers.wrappers import TimeDistributed
 from keras.layers.noise import GaussianNoise
-from keras.optimizers import SGD
+from keras.optimizers import SGD, RMSprop, Adadelta
 from keras.models import model_from_json
-from feats import myGenerator, get_batches
+from feats import myGenerator
 
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
 
 EMBEDDINGS_DIMENSION = 50
 NUM_CLASSES = 3
-INPUT_SAMPLE_SIZE=12900
 
 def save_model(model, filename):
     # serialize model to JSON
@@ -43,25 +43,34 @@ def load_model(filename):
     print("Loaded model from disk")
     return loaded_model
 
+def get_dims(generator, embedding_size):
+    inp, out = next(generator)
+    inp_shape = (None, inp.shape[-1])
+    out_shape = (None, out.shape[-1]//NUM_CLASSES * embedding_size)
+    return inp_shape, out_shape
+    
     
 def affinitykmeans(Y, V):
     def norm(tensor):
-        square_tensor = tf.square(tensor)
-        tensor_sum = tf.reduce_sum(square_tensor)
-        frobenius_norm = tf.sqrt(tensor_sum)
+        square_tensor = K.square(tensor)
+        tensor_sum = K.sum(square_tensor)
+        frobenius_norm = K.sqrt(tensor_sum)
         return frobenius_norm
-    
+
     # V e Y estao vetorizados
     # Antes de mais nada, volto ao formato de matrizes
-    V = tf.reshape(V, [-1, EMBEDDINGS_DIMENSION])
-    Y = tf.reshape(Y, [-1, NUM_CLASSES])
-   
-    T = tf.transpose
-    dot = tf.matmul
+    V = K.reshape(V, [-1, EMBEDDINGS_DIMENSION])
+    Y = K.reshape(Y, [-1, NUM_CLASSES])
+
+    T = K.transpose
+    dot = K.dot
     return norm(dot(T(V), V)) - 2 * norm(dot(T(V), Y)) + norm(dot(T(Y), Y))
 
 
-def train_nnet():       
+
+def train_nnet():
+    inp_shape, out_shape = get_dims(myGenerator(),
+                                    EMBEDDINGS_DIMENSION)
 #    model = Sequential()
 #    model.add(Dense(64, input_dim=INPUT_SAMPLE_SIZE, init='uniform'))
 #    model.add(Activation('tanh'))
@@ -73,49 +82,51 @@ def train_nnet():
 #    model.add(Activation('softmax'))
 
     model = Sequential()
-    model.add(Bidirectional(LSTM(300, return_sequences=True), input_shape=(100,129)))
-    #model.add(Dropout(0.5))
-    #model.add(GaussianNoise(0.77))    
-    model.add(Bidirectional(LSTM(120)))    
-    #model.add(GaussianNoise(0.77))
-    #model.add(Dropout(0.5))
-    model.add(Dense(INPUT_SAMPLE_SIZE * EMBEDDINGS_DIMENSION, init='uniform', activation='tanh'))
+    model.add(Bidirectional(LSTM(30, return_sequences=True),
+                            input_shape=inp_shape))
+    model.add(TimeDistributed(BatchNormalization(mode=2)))
+#    model.add(GaussianNoise(0.77))
+#    model.add(Dropout(0.5))
+    model.add(Bidirectional(LSTM(30, return_sequences=True)))
+    model.add(TimeDistributed(BatchNormalization(mode=2)))
+#    model.add(GaussianNoise(0.77))
+#    model.add(Dropout(0.5))
+    model.add(TimeDistributed(Dense(out_shape[-1],
+                                    init='uniform',
+                                    activation='tanh')))
+#    model.add(Reshape((12900,EMBEDDINGS_DIMENSION)))
+
+
     #model.add(TimeDistributed(Dense(EMBEDDINGS_DIMENSION)))
     #model.add(Activation('softmax'))
 
-    sgd = SGD(lr=1e-5, momentum=0.9, decay=0.0, nesterov=True)
+#    sgd = SGD(lr=1e-5, momentum=0.9, decay=0.0, nesterov=True)
+    sgd = Adadelta()
     model.compile(loss=affinitykmeans, optimizer=sgd)
 
-    model.fit_generator(myGenerator(),  samples_per_epoch=50, nb_epoch=10, max_q_size=100)
-    #score = model.evaluate(X_test, y_test, batch_size=16)
+    model.fit_generator(myGenerator(),
+                        samples_per_epoch=20, nb_epoch=10, max_q_size=10)
+    # score = model.evaluate(X_test, y_test, batch_size=16)
     save_model(model, "model")
     
+
 def main():
-    train_nnet()    
+    train_nnet()
     loaded_model = load_model("model")
 
-    xval = []
-    yref = []
-    ypred = []
-    i = 0
-    for x, y in myGenerator():
-        i += 1
-        #if i % 2 == 1:
-        v = loaded_model.predict(x)
-        xval.append(x.reshape(100, 129)[:50])
-        yref.append(y.reshape(100, 129, NUM_CLASSES)[:50])
-        ypred.append(v.reshape(100, 129, EMBEDDINGS_DIMENSION)[:50])
-        if i == 6:
-            break
-#    #with open("outfile", "w") as f:
-    xval = np.concatenate(xval)
-    yref = np.concatenate(yref)
-    ypred = np.concatenate(ypred)
+    x, y = next(myGenerator())
+    v = loaded_model.predict(x)
+    x = x[0][::2]
+    y = y[0][::2]
+    v = v[0][::2]
+    x = x.reshape((-1, 129))
+    y = y.reshape((-1, 129, NUM_CLASSES))
+    v = v.reshape((-1, 129, EMBEDDINGS_DIMENSION))
 
     k = NUM_CLASSES
     model = KMeans(k)
-    eg = model.fit_predict(ypred.reshape(ypred.size//50, 50))
-    imshape = yref.shape
+    eg = model.fit_predict(v.reshape(-1, EMBEDDINGS_DIMENSION))
+    imshape = y.shape
     img = np.zeros(eg.shape + (3,))
     img[eg == 0] = [1, 0, 0]
     img[eg == 1] = [0, 1, 0]
@@ -124,8 +135,8 @@ def main():
         img[eg == 3] = [0, 0, 0]
     img = img.reshape(imshape)
 
-    img2 = yref
-    img3 = xval
+    img2 = y
+    img3 = x
 
     fig, (ax1, ax2, ax3) = plt.subplots(3, 1)
     ax1.imshow(img.swapaxes(0, 1), origin='lower')
