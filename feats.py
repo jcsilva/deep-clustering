f024640c4875a6b061d97e36dc526911887b46f5
Eduardo Silva <zedudu@gmail.com>
2016-10-04 15:38:44 -0300

    Refatoração geral da implementação

--- feats.py
+++ feats.py
@@ -5,13 +5,16 @@ Created on Mon Sep 26 15:23:31 2016
 @author: jcsilva
 """
 import numpy as np
+import random
 import scipy.io.wavfile as wav
 from python_speech_features import sigproc
-from keras.utils.np_utils import to_categorical
+
 
 FRAME_LENGTH = .032
 FRAME_SHIFT = .008
+FS = 8000
 CONTEXT = 100
+
 
 def squared_hann(M):
     return np.sqrt(np.hanning(M))
@@ -23,73 +26,134 @@ def stft(sig, rate):
                               FRAME_SHIFT*rate,
                               winfunc=squared_hann)
     spec = np.fft.rfft(frames, int(FRAME_LENGTH*rate))
-    return np.log(np.absolute(spec))
+    return np.real(np.log10(spec))  # Log 10 for easier dB calculation
 
 
-def get_signals(min_idx=1, max_idx=700):
-    root_prefix = "/media/data/corpora/Laps/LapsBM1.4-8k/all/LapsBM_"   
-    file_num = str(np.random.randint(min_idx, max_idx))
-    #file_num = "61"
-    p1 = root_prefix + file_num.rjust(4,'0') + ".wav"
-    rate, sig = wav.read(p1)
-    sig = sig - np.mean(sig)
-    sig = sig/np.max(np.abs(sig))
+def get_egs(wavlist, min_mix=2, max_mix=3, sil_as_class=True, batch_size=1):
+    """
+    Generate examples for the neural network from a list of wave files with
+    speaker ids. Each line is of type "path speaker", as follows:
 
-    file_num = str(np.random.randint(min_idx, max_idx))
-    #file_num = "1"
-    p2 = root_prefix + file_num.rjust(4,'0') + ".wav"
-    rate2, sig2 = wav.read(p2)
-    sig2 = sig2 - np.mean(sig2)
-    sig2 = sig2/np.max(np.abs(sig2))
-    
-    smallest_signal_len = len(sig) if len(sig) < len(sig2) else len(sig2)
-    
-    x = sig[:smallest_signal_len] + sig2[:smallest_signal_len]
+    path/to/1st.wav spk1
+    path/to/2nd.wav spk2
+    path/to/3rd.wav spk1
 
-    S1 = stft(sig[:smallest_signal_len], rate)
-    S2 = stft(sig2[:smallest_signal_len], rate2)
-    X = stft(x, rate) #[TODO] vou tratar de caso com taxa de amostragem diferente?
-    
-    return S1, S2, X
-    
-    
-def myGenerator(min_idx=1, max_idx=700, n=0):
-    counter = 0 
-    
-    def condition(counter, n):
-        if n > 0:
-            return counter < n
+    and so on.
+    min_mix and max_mix are the minimum and maximum number of examples to
+    be mixed for generating a training example
+
+    sil_as_class defines if the threshold-defined background silence will
+    be treated as a separate class
+    """
+    speaker_wavs = {}
+    batch_x = []
+    batch_y = []
+    batch_count = 0
+    while True:  # Generate examples indefinitely
+        # Select number of files to mix
+        k = np.random.randint(min_mix, max_mix+1)
+        if k > len(speaker_wavs):
+            # Reading wav files list and separating per speaker
+            speaker_wavs = {}
+            f = open(wavlist)
+            for line in f:
+                line = line.strip().split()
+                if len(line) != 2:
+                    continue
+                p, spk = line
+                if spk not in speaker_wavs:
+                    speaker_wavs[spk] = []
+                speaker_wavs[spk].append(p)
+            f.close()
+            # Randomizing wav lists
+            for spk in speaker_wavs:
+                random.shuffle(speaker_wavs[spk])
+        wavsum = None
+        sigs = []
+
+        # Pop wav files from random speakers, store them individually for
+        # dominant spectra decision and generate the mixed input
+        for spk in random.sample(speaker_wavs.keys(), k):
+            p = speaker_wavs[spk].pop()
+            if not speaker_wavs[spk]:
+                del(speaker_wavs[spk])  # Remove empty speakers from dictionary
+            rate, sig = wav.read(p)
+            sig = sig - np.mean(sig)
+            sig = sig/np.max(np.abs(sig))
+            sig *= (np.random.random()*1/4 + 3/4)
+            if wavsum is None:
+                wavsum = sig
+            else:
+                wavsum = wavsum[:len(sig)] + sig[:len(wavsum)]
+            sigs.append(sig)
+
+        # STFT for mixed signal
+        X = np.real(stft(wavsum, rate))
+        if len(X) <= CONTEXT:
+            continue
+
+        # STFTs for individual signals
+        specs = []
+        for sig in sigs:
+            specs.append(stft(sig[:len(wavsum)], rate))
+        specs = np.array(specs)
+
+        if sil_as_class:
+            nc = k + 1
         else:
-            return True
-            
-    while condition(counter, n):
-        counter = counter + 1
-        # escolho um arquivo
-        S1, S2, X = get_signals(min_idx,max_idx)
-        
-        # cria mascara binaria
-        m = np.max(X) - 5.5 # consertar esse valor. Isso eh soh para identificar silencio!!
-        y = np.zeros(X.shape, dtype=np.int16)
-        y[S1 > S2] = 0
-        y[S2 >= S1] = 1
-        y[X < m] = 2
+            nc = k
 
-        total_frames = X.shape[0]
-        idx = 0
-        total_x = []
-        total_y = []
-        while (idx + CONTEXT) < total_frames:
-            # blstm
-            # ACUMULAR ENTRADA E SAIDA DA FORMA: (1, n, 12900), (n, 12900*n_classes),
-            # sendo n o numero de iteracoes por esse loop
-            total_x.append(X[idx:idx + CONTEXT])
-            total_y.append(to_categorical(np.ravel(y[idx:idx + CONTEXT])).reshape((100,-1)))
-            idx = idx + CONTEXT // 2
-            #yield(np.array(total_x).transpose([1,0,2]), np.array(total_y).transpose([1,0,2]))
-        yield(np.array(total_x), np.array(total_y))
+        # Get dominant spectra indexes, create one-hot outputs
+        Y = np.zeros(X.shape + (nc,))
+        vals = np.argmax(specs, axis=0)
+        for i in range(k):
+            t = np.zeros(nc)
+            t[i] = 1
+            Y[vals == i] = t
 
-        
+        # EXPERIMENTAL: normalize mag spectra as weighted norm vectors instead
+        # of using unit vectors for "hard" classes
+#        if sil_as_class:
+#            print("This won't work with sil_as_class=True")
+#        from sklearn.preprocessing import normalize
+#        Y = np.transpose(specs, (1, 2, 0))
+#        Y = Y.reshape((-1, nc))
+#        Y = normalize(Y, axis=1)
+#        Y = Y.reshape(X.shape + (nc,))
+
+        # Create mask for zeroing out gradients from silence components
+        m = np.max(X) - 40./20  # Minus 40dB
+        if sil_as_class:
+            z = np.zeros(nc)
+            z[-1] = 1
+            Y[X < m] = z
+        else:
+            z = np.zeros(nc)
+            Y[X < m] = z
+        i = 0
+
+        # Generating sequences
+        while i + CONTEXT < len(X):
+            batch_x.append(X[i:i+CONTEXT])
+            batch_y.append(Y[i:i+CONTEXT])
+            i += CONTEXT//2
+
+            batch_count = batch_count+1
+
+            if batch_count == batch_size:
+                yield(np.array(batch_x).reshape((batch_size, CONTEXT, -1)),
+                      np.array(batch_y).reshape((batch_size, CONTEXT, -1)))
+                batch_x = []
+                batch_y = []
+                batch_count = 0
+
+
 if __name__ == "__main__":
-    a = myGenerator(1)
-    for i,j in a:
-        print(i.shape,j.shape)
+    a = get_egs('wavlist_short', 2, 2, False)
+    k = 6
+    for i, j in a:
+        print(i.shape, j.shape)
+        print(j[0][0])
+        k -= 1
+        if k == 0:
+            break
--- keras-speech-separation-batch.py
+++ keras-speech-separation-batch.py
@@ -7,21 +7,92 @@ Created on Mon Sep 26 15:23:31 2016
 
 from keras import backend as K
 from keras.models import Sequential
-from keras.layers import Dropout, Activation, Input, Reshape, BatchNormalization
-from keras.layers import Dense, LSTM, Bidirectional
-from keras.layers.wrappers import TimeDistributed
+from keras.layers import Dropout, Activation, Input, Reshape
+from keras.layers import Dense, LSTM, BatchNormalization
+from keras.layers import TimeDistributed, Bidirectional
 from keras.layers.noise import GaussianNoise
-from keras.optimizers import SGD, RMSprop, Adadelta
+from keras.optimizers import Adam, SGD, Adadelta
 from keras.models import model_from_json
-from keras.callbacks import EarlyStopping, ModelCheckpoint
-from feats import myGenerator
-
+from keras.callbacks import ModelCheckpoint
+from feats import get_egs
 import numpy as np
-from sklearn.cluster import KMeans
-import matplotlib.pyplot as plt
 
-EMBEDDINGS_DIMENSION = 50
-NUM_CLASSES = 3
+EMBEDDINGS_DIMENSION = 20
+NUM_CLASSES = 2
+SIL_AS_CLASS = False
+
+BATCH_SIZE = 10
+TIMESTEPS = 100
+FREQSTEPS = 129
+
+
+def print_examples(x, y, v):
+    from sklearn.cluster import KMeans
+    from itertools import permutations
+    import matplotlib.pyplot as plt
+    from sklearn.preprocessing import normalize
+
+    x = x[0][::2]
+    y = y[0][::2]
+    v = v[0][::2]
+
+    v = normalize(v, axis=1)
+    k = NUM_CLASSES + int(SIL_AS_CLASS)
+
+    x = x.reshape((-1, 129))
+    y = y.reshape((-1, 129, k))
+    v = v.reshape((-1, 129, EMBEDDINGS_DIMENSION))
+
+    model = KMeans(k)
+    eg = model.fit_predict(v.reshape(-1, EMBEDDINGS_DIMENSION))
+    imshape = x.shape + (3,)
+    img = np.zeros(eg.shape + (3,))
+    img[eg == 0] = [1, 0, 0]
+    img[eg == 1] = [0, 1, 0]
+    if(k > 2):
+        img[eg == 2] = [0, 0, 1]
+        img[eg == 3] = [0, 0, 0]
+    img = img.reshape(imshape)
+
+    img2 = np.zeros(eg.shape + (3,))
+    vals = np.argmax(y.reshape((-1, k)), axis=1)
+    print(img2.shape, vals.shape)
+    for i in range(k):
+        t = np.zeros(3)
+        t[i] = 1
+        img2[vals == i] = t
+    img2 = img2.reshape(imshape)
+
+    img3 = x
+
+    # Find most probable color permutation from prediction
+    p = None
+    s = np.float('Inf')
+    for pp in permutations([0, 1, 2]):
+        ss = np.sum(np.square(img2 - img[:, :, pp]))
+        if ss < s:
+            s = ss
+            p = pp
+    img = img[:, :, p]
+    img4 = 1 - (((img-img2+1)/2))
+    img4[np.all(img4 == .5, axis=2)] = 0
+
+    fig, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1)
+    ax1.imshow(img.swapaxes(0, 1), origin='lower')
+    ax2.imshow(img2.swapaxes(0, 1), origin='lower')
+    ax3.imshow(img4.swapaxes(0, 1), origin='lower')
+    ax4.imshow(img3.swapaxes(0, 1), origin='lower')
+
+
+def get_dims(generator, embedding_size):
+    inp, out = next(generator)
+    k = NUM_CLASSES + int(SIL_AS_CLASS)
+    inp_shape = inp.shape[1:]
+    out_shape = list(out.shape[1:])
+    out_shape[-1] *= embedding_size/k
+    out_shape[-1] = int(out_shape[-1])
+    out_shape = tuple(out_shape)
+    return inp_shape, out_shape
 
 
 def save_model(model, filename):
@@ -46,109 +117,122 @@ def load_model(filename):
     return loaded_model
 
 
-def get_dims(generator, embedding_size):
-    inp, out = next(generator)
-    inp_shape = (None, inp.shape[-1])
-    out_shape = (None, out.shape[-1]//NUM_CLASSES * embedding_size)
-    return inp_shape, out_shape
-
-
 def affinitykmeans(Y, V):
     def norm(tensor):
         square_tensor = K.square(tensor)
-        frobenius_norm2 = K.sum(square_tensor)
-        frobenius_norm = K.sqrt(frobenius_norm2)
+        tensor_sum = K.sum(square_tensor, axis=(1, 2))
+        frobenius_norm = K.sqrt(tensor_sum)
         return frobenius_norm
+
+    def dot(x, y):
+        return K.batch_dot(x, y, axes=(2, 1))
+
+    def T(x):
+        return K.permute_dimensions(x, [0, 2, 1])
 
     # V e Y estao vetorizados
     # Antes de mais nada, volto ao formato de matrizes
-    V = K.l2_normalize(K.reshape(V, [-1, EMBEDDINGS_DIMENSION]), axis=1)
-    Y = K.reshape(Y, [-1, NUM_CLASSES])
+    V = K.l2_normalize(K.reshape(V, [BATCH_SIZE,
+                                     TIMESTEPS*FREQSTEPS,
+                                     EMBEDDINGS_DIMENSION]), axis=-1)
+    Y = K.reshape(Y, [BATCH_SIZE,
+                      TIMESTEPS*FREQSTEPS,
+                      NUM_CLASSES + int(SIL_AS_CLASS)])
+    
+    mask = K.sum(Y, axis=2, keepdims=True)
+    V = mask * V     
+    
+    return norm(dot(T(V), V)) - norm(dot(T(V), Y)) * 2 + norm(dot(T(Y), Y))
 
-    T = K.transpose
-    dot = K.dot
-    return norm(dot(T(V), V)) - 2 * norm(dot(T(V), Y)) + norm(dot(T(Y), Y))
 
-
-def train_nnet():
-    inp_shape, out_shape = get_dims(myGenerator(),
+def train_nnet(train_list, valid_list, weights_path=None):
+    train_gen = get_egs(train_list,
+                        min_mix=NUM_CLASSES,
+                        max_mix=NUM_CLASSES,
+                        sil_as_class=SIL_AS_CLASS,
+                        batch_size=BATCH_SIZE)
+    valid_gen = get_egs(valid_list,
+                        min_mix=NUM_CLASSES,
+                        max_mix=NUM_CLASSES,
+                        sil_as_class=SIL_AS_CLASS,
+                        batch_size=BATCH_SIZE)
+    inp_shape, out_shape = get_dims(train_gen,
                                     EMBEDDINGS_DIMENSION)
-#    model = Sequential()
-#    model.add(Dense(64, input_dim=INPUT_SAMPLE_SIZE, init='uniform'))
-#    model.add(Activation('tanh'))
-#    model.add(Dropout(0.5))
-#    model.add(Dense(64, init='uniform'))
-#    model.add(Activation('tanh'))
-#    model.add(Dropout(0.5))
-#    model.add(Dense(INPUT_SAMPLE_SIZE * EMBEDDINGS_DIMENSION, init='uniform'))
-#    model.add(Activation('softmax'))
-
     model = Sequential()
-    model.add(Bidirectional(LSTM(600, return_sequences=True),
+    # model.add(BatchNormalization(mode=2, input_shape=inp_shape))
+    model.add(Bidirectional(LSTM(120, return_sequences=True,
+                                 activation='tanh'),
                             input_shape=inp_shape))
-    model.add(TimeDistributed(BatchNormalization(mode=2)))
-    model.add(GaussianNoise(0.77))
-    # model.add(Dropout(0.5))
-    model.add(Bidirectional(LSTM(600, return_sequences=True)))
-    model.add(TimeDistributed(BatchNormalization(mode=2)))
-    model.add(GaussianNoise(0.77))
-    # model.add(Dropout(0.5))
+    # model.add(TimeDistributed(BatchNormalization(mode=2)))
+    model.add(TimeDistributed((GaussianNoise(0.775))))
+#    model.add(TimeDistributed((Dropout(0.5))))
+    model.add(Bidirectional(LSTM(120, return_sequences=True,
+                                 activation='tanh')))
+    # model.add(TimeDistributed(BatchNormalization(mode=2)))
+    model.add(TimeDistributed((GaussianNoise(0.775))))
+#    model.add(TimeDistributed((Dropout(0.5))))
     model.add(TimeDistributed(Dense(out_shape[-1],
                                     init='uniform',
                                     activation='tanh')))
 
-    sgd = RMSprop()
+#    sgd = SGD(lr=1e-5, momentum=0.9, decay=0.0, nesterov=True)
+    sgd = Adadelta()
+    if weights_path:
+        model.load_weights(weights_path)
 
     model.compile(loss=affinitykmeans, optimizer=sgd)
 
-    earlyStopping = EarlyStopping(monitor='val_loss', patience=5)
-
-    checkpoint = ModelCheckpoint('weights-{epoch:03d}-{val_loss:.2f}.hdf5',
+    # checkpoint
+    filepath = "weights-improvement-{epoch:02d}-{val_loss:.2f}.h5"
+    checkpoint = ModelCheckpoint(filepath,
+                                 monitor='val_loss',
+                                 verbose=0,
+                                 save_best_only=True,
+                                 mode='min',
                                  save_weights_only=True)
 
-    model.fit_generator(myGenerator(1, 600),
-                        samples_per_epoch=800,
-                        nb_epoch=200,
-                        max_q_size=800,
-                        validation_data=myGenerator(601, 700),
-                        nb_val_samples=100,
-                        callbacks=[earlyStopping, checkpoint])
+    callbacks_list = [checkpoint]
+
+    model.fit_generator(train_gen,
+                        validation_data=valid_gen,
+                        nb_val_samples=1,
+                        samples_per_epoch=200,
+                        nb_epoch=50,
+                        max_q_size=10,
+                        callbacks=callbacks_list)
     # score = model.evaluate(X_test, y_test, batch_size=16)
     save_model(model, "model")
 
 
 def main():
-    train_nnet()
+    train_nnet('wavlist_short', 'wavlist_short')
     loaded_model = load_model("model")
+    X = []
+    Y = []
+    V = []
+    gen = get_egs('wavlist_short', 2, 2, SIL_AS_CLASS)
+    i = 0
+    for inp, ref in gen:
+        inp, ref = next(gen)
+        X.append(inp)
+        Y.append(ref)
+        V.append(loaded_model.predict(inp))
+        i += 1
+        if i == 8:
+            break
+    x = np.concatenate(X, axis=1)
+    y = np.concatenate(Y, axis=1)
+    v = np.concatenate(V, axis=1)
 
-    x, y = next(myGenerator(601, 700))
-    v = loaded_model.predict(x)
-    x = x[:][::2]
-    y = y[:][::2]
-    v = v[:][::2]
-    x = x.reshape((-1, 129))
-    y = y.reshape((-1, 129, NUM_CLASSES))
-    v = v.reshape((-1, 129, EMBEDDINGS_DIMENSION))
+#    np.save('x', x)
+#    np.save('y', y)
+#    np.save('v', v)
+#
+#    x = np.load('x.npy')
+#    y = np.load('y.npy')
+#    v = np.load('v.npy')
+    print_examples(x, y, v)
 
-    k = NUM_CLASSES
-    model = KMeans(k)
-    eg = model.fit_predict(v.reshape(-1, EMBEDDINGS_DIMENSION))
-    imshape = y.shape
-    img = np.zeros(eg.shape + (3,))
-    img[eg == 0] = [1, 0, 0]
-    img[eg == 1] = [0, 1, 0]
-    if(k > 2):
-        img[eg == 2] = [0, 0, 1]
-        img[eg == 3] = [0, 0, 0]
-    img = img.reshape(imshape)
-
-    img2 = y
-    img3 = x
-
-    fig, (ax1, ax2, ax3) = plt.subplots(3, 1)
-    ax1.imshow(img.swapaxes(0, 1), origin='lower')
-    ax2.imshow(img2.swapaxes(0, 1), origin='lower')
-    ax3.imshow(img3.swapaxes(0, 1), origin='lower')
 
 if __name__ == "__main__":
     main()
