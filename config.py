8f7b37a24dd300f6ad8b1fad5aecb93f9ffb6f05
apmig <apmig@cpqd.com.br>
2016-10-14 15:01:54 -0300

    Corrige nome de parâmetros em config.py

--- config.py
+++ config.py
@@ -8,18 +8,18 @@ TIMESTEPS = 100
 # Clustering parameters
 EMBEDDINGS_DIMENSION = 20
 MIN_MIX = 2
-MAX_MIX = 2
+MAX_MIX = 3
 SIL_AS_CLASS = False
 
 # Neural net topology
-LSTM_SIZE = 30
-NUM_LSTMS = 2
+SIZE_RLAYERS = 300
+NUM_RLAYERS = 2
 
 # Training parameters
-BATCH_SIZE = 1
-SAMPLES_PER_EPOCH = 100
-NUM_EPOCHS = 1
-VALID_SIZE = 80
+BATCH_SIZE = 256
+SAMPLES_PER_EPOCH = 131072
+NUM_EPOCHS = 50
+VALID_SIZE = 32768
 
 # Regularization parameters
 DROPOUT = 0.2   # Feed forward dropout
