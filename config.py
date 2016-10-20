bd9621605583bf54475e8c44389ad583f9c9dbd4
Valter Akira Miasato Filho <valterf@cpqd.com.br>
2016-10-20 15:06:03 -0200

    Modifica configurações padrão de treinamento

--- config.py
+++ config.py
@@ -6,23 +6,23 @@ FRAME_SHIFT = .008
 TIMESTEPS = 100
 
 # Clustering parameters
-EMBEDDINGS_DIMENSION = 20
+EMBEDDINGS_DIMENSION = 40
 MIN_MIX = 2
-MAX_MIX = 2
+MAX_MIX = 3
 SIL_AS_CLASS = False
 
 # Neural net topology
 SIZE_RLAYERS = 300
-NUM_RLAYERS = 2
+NUM_RLAYERS = 4
 
 # Training parameters
-BATCH_SIZE = 1
-SAMPLES_PER_EPOCH = 300
-NUM_EPOCHS = 1
-VALID_SIZE = 10
+BATCH_SIZE = 128
+SAMPLES_PER_EPOCH = 4096
+NUM_EPOCHS = 200
+VALID_SIZE = 512
 
 # Regularization parameters
 DROPOUT = 0.5   # Feed forward dropout
 RDROPOUT = 0.2  # Recurrent dropout
-L2R = 1e-3      # L2 regularization factor
+L2R = 1e-6      # L2 regularization factor
 CLIPNORM = 200  # Norm clipping for gradients
